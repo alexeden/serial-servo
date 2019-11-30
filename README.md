0068879e89d15f2626379f7a2f75895f23469207
Alex Eden <alexandereden91@gmail.com>
2019-11-30 00:26:19 -0600

    It works! Add notes to readme about troubleshooting on dietpi

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ If permissions prevent opening AMA0 port, run this:
 ```
 sudo chmod 666 /dev/ttyAMA0
 ```
+
+On dietpi, if not getting data from the serial port, run `sudo dietpi-config`, go to _Advanced Options_ > _Serial/UART_, make sure `ttyS0 console` and `ttyAMA0 console` are both **off**.
--- examples/test.ts
+++ examples/test.ts
@@ -1,12 +1,10 @@
 import * as SerialPort from 'serialport';
 
-console.log('SerialPort: ', SerialPort);
-
 const portOpts: SerialPort.OpenOptions = {
-  // baudRate: 115200,
+  baudRate: 115200,
 };
 
-const port = new SerialPort('/dev/serial0', portOpts, error => {
+const port = new SerialPort('/dev/ttyAMA0', portOpts, error => {
   if (error) {
     console.log('Error opening the port: ', error);
   }
@@ -17,47 +15,53 @@ const readPosPackage = Buffer.of(
   0x55,
   0x01,
   0x03,
-  28,
-  0xFF & ~(0x01 + 0x03 + 28)
+  0x1c,
+  0xFF & ~(0x01 + 0x03 + 0x1c)
 );
 
 port.on('data', data => {
-  console.log('Data:', data);
+  console.log('Data event:', data);
 });
 
-const writeReadPosPackage = () => port.write(readPosPackage, (error, bytesWritten, ...args) => {
-  console.log('Error: ', error, 'bytes written: ', bytesWritten, ...args);
-  if (error) {
-    console.error('Write error: ', error);
-  }
-  else {
-    console.log(`${bytesWritten} bytes written to port`);
-  }
-});
+const writeReadPosPackage = () => {
+  console.log('writing packet');
+  return new Promise((ok, err) => {
+    port.write(readPosPackage, (error, bytesWritten, ...args) => {
+      if (error) {
+        console.error('Write error: ', error);
+        err(error);
+      }
+      else {
+        console.log(`${bytesWritten} bytes written to port`);
+        ok(bytesWritten);
+      }
+    });
+  });
+};
 
 // Open errors will be emitted as an error event
 port.on('error', error => {
-  console.log('Error: ', error.message);
+  console.log('Port error event: ', error.message);
 });
 
 const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
 (async () => {
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
-  console.log(writeReadPosPackage());
+  await writeReadPosPackage();
   await wait(1000);
 })();
