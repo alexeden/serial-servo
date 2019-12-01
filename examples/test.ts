bb88cc717705e67beefc0e5b082078700e4abf14
Alex Eden <alexandereden91@gmail.com>
2019-12-01 01:46:15 -0600

    Add call to get angle limit to test script

--- examples/test.ts
+++ examples/test.ts
@@ -13,10 +13,16 @@ const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
   console.log(await SerialPort.list());
   const platform = await ServoPlatform.ofPath('/dev/ttyAMA0');
 
+  console.log(await platform.sendCommand(CommandGenerator.getAngleLimits(1)));
   console.log(await platform.sendCommand(CommandGenerator.getAngle(1)));
+  console.log(await platform.sendCommand(CommandGenerator.getAngleLimits(2)));
   console.log(await platform.sendCommand(CommandGenerator.getAngle(2)));
+  console.log(await platform.sendCommand(CommandGenerator.getAngleLimits(3)));
   console.log(await platform.sendCommand(CommandGenerator.getAngle(3)));
+  console.log(await platform.sendCommand(CommandGenerator.getAngleLimits(4)));
   console.log(await platform.sendCommand(CommandGenerator.getAngle(4)));
+  console.log(await platform.sendCommand(CommandGenerator.getAngleLimits(5)));
   console.log(await platform.sendCommand(CommandGenerator.getAngle(5)));
+  console.log(await platform.sendCommand(CommandGenerator.getAngleLimits(6)));
   console.log(await platform.sendCommand(CommandGenerator.getAngle(6)));
 })();
